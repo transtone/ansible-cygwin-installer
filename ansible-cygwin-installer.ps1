ef50b886b6e6e8579d18578c69ec52ea79774cd7
transtone <zm3345@126.com>
2020-02-03 21:37:42 +0800

    chore: use apt-cyg

--- ansible-cygwin-installer.ps1
+++ ansible-cygwin-installer.ps1
@@ -16,8 +16,7 @@
 $storageDir = $pwd
 $cygwinHome = "c:\cygwin"
 $cygwinUrlRoot = "http://cygwin.com"
-$getPipUrlRoot = "https://bootstrap.pypa.io"
-$cygwinMirror = "http://cygwin.mirrors.pair.com"
+$cygwinMirror = "https://mirrors.tuna.tsinghua.edu.cn/cygwin"
 
 #
 # You shouldn't normally need to change anything below here
@@ -45,21 +44,23 @@ if ( ! ( Test-Path -Path $cygwinSetupPath -PathType Leaf ) ) {
     $webclient.DownloadFile($url,$file)
 }
 
-$cygwinSetupArgs = '--no-admin', '-q', '-R', "$cygwinHome", '-s', "$cygwinMirror", '--packages="wget,python,git,vim,openssh,openssl,openssh-devel,libsasl2,ca-certificates,python-crypto,python-openssl,python-setuptools,dash,rebase"'
+$cygwinSetupArgs = '--no-admin', '-q', '-R', "$cygwinHome", '-s', "$cygwinMirror", '--packages="binutils,wget,dash"'
 Start-Process -FilePath $cygwinSetupPath -ArgumentList $cygwinSetupArgs -Wait
 
 # Add cygwin bin dir to path
 $ENV:PATH="$cygwinHome\bin;$ENV:PATH"
 
 # Install pip
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', """wget.exe $getPipUrlRoot/get-pip.py""" -Wait -NoNewWindow
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"python2.7.exe get-pip.py"' -Wait -NoNewWindow
+Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', """install apt-cyg /bin""" -Wait -NoNewWindow
 
-# Fix fork() errors on some systems
-Start-Process -FilePath $cygwinHome\bin\dash.exe -ArgumentList '-c', '"/usr/bin/rebaseall -v"' -Wait -NoNewWindow
+Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', """apt-cyg install curl git vim gmp libffi-devel libgmp-devel nano openssh openssl-devel ansible """ -Wait -NoNewWindow
+
 
 # Install Ansible via pip
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"pip2.7 install ansible"' -Wait -NoNewWindow
+
+# Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"pip2 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U"' -Wait -NoNewWindow
+
+# Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"pip2 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple"' -Wait -NoNewWindow
 
 # Run Ansible from outside of Cygwin shell
 Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"ansible --version"' -Wait -NoNewWindow
